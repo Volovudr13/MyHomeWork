d102a39e59f5e23664c69767d1d8c9b7e660bf6b
Vladimir <_Vladimir210@yandex.ru>
2022-04-19 22:12:52 +0300

    My second C# home work

---
+++ HW2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 1.  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+/*
+void SecondNum()
+{
+    int num = new Random().Next(100,999);
+    Console.WriteLine(num);
+    int num1 = num %100;
+    int num2 = num1 / 10;
+    Console.WriteLine(num1);
+    Console.WriteLine(num2);
+
+}
+
+SecondNum();
+*/
+
+// Задача 2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+/*
+void Aaaaa()
+
+{   
+    int q = new Random().Next(0,999);
+    Console.WriteLine(q);
+
+    if (q >= 100)
+    {
+  
+        q = (q % 100) % 10;
+
+        Console.WriteLine(q);
+    }
+    else
+    {
+        Console.WriteLine("Третьего числа нет");
+
+    }
+}
+
+Aaaaa();
+*/
+
+
+// Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+/*
+int q = new Random().Next(0,8);
+if (q > 5 && q < 8)
+{
+    Console.WriteLine(q + " Is Off day");
+}
+else
+{
+    Console.WriteLine(q + " This day is not of off day");
+}
+
+*/
+
