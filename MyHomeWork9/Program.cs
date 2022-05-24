b02f523546047073b594b0fb7929f67d9526ca6c
Vladimir <_Vladimir210@yandex.ru>
2022-05-24 15:40:34 +0300

    My ninth home work

---
+++ MyHomeWork9/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+/*
+int SumNums (int m, int n)
+{
+    if (m<= n)
+    {
+       return m + SumNums(m+1,n); 
+    }
+    else
+        return 0;
+}
+Console.Write(SumNums(1,15));
+*/
+
+
+
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+/*
+int SumNums (int m)
+{
+    if (m < 10)
+       return m; 
+
+    return m%10  + SumNums(m/10); 
+    
+}
+
+Console.Write(SumNums(777));
+*/
